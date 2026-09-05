4b218332bf723f68707ca76ec3ad5271f22d7eef
IKEGAMI-99 <hato.jp@gmail.com>
2026-09-06 08:46:37 +0900

    Bump version to 0.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.ikegami99.realityscanner"
         minSdk = 28
         targetSdk = 37
-        versionCode = 7
-        versionName = "0.1.6"
+        versionCode = 8
+        versionName = "0.1.7"
 
         buildConfigField("String", "GITHUB_REPO", "\"IKEGAMI-99/REALITY-SCANNER\"")
     }
